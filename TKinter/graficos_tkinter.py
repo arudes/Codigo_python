d3d14b17fcef291fd08df0d72573fe4924fdeca0
Mauricio Alberto Maldonado Narvaez <mmaldonado192090@gmail.com>
2020-02-26 21:19:42 -0600

    modificado con comentarios

--- TKinter/graficos_tkinter.py
+++ TKinter/graficos_tkinter.py
@@ -6,9 +6,9 @@ Created on Wed Feb 26 17:22:02 2020
 """
 from tkinter import *
 
-principal = Tk()
-principal.title("Hola Mundo")
-principal.resizable(1,1)
-principal.iconbitmap('python.ico')
+principal = Tk() #se crea el marco
+principal.title("Hola Mundo") # se le asigna un nombre al titulo de la ventana
+principal.resizable(1,1) #se le asigna permisos para redimencionar
+principal.iconbitmap('python.ico') #se fija un icono de pantalla
 
-principal.mainloop()
+principal.mainloop() #se crea un bucle que permite ver la pantalla
