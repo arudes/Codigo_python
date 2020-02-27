f41b589a151c5e7099f7d2c95ccd0d992c6d22d6
Mauricio Alberto Maldonado Narvaez <mmaldonado192090@gmail.com>
2020-02-26 20:53:42 -0600

    Inicio de subida de archivos

---
+++ TKinter/graficos_tkinter.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 26 17:22:02 2020
+
+@author: Maldonado
+"""
+from tkinter import *
+
+principal = Tk()
+principal.title("Hola Mundo")
+principal.resizable(1,1)
+principal.iconbitmap('python.ico')
+
+principal.mainloop()
